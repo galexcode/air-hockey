83b6a6e33e69e9728b71896d0453091ac264c8ce
Krzysztof <cypismail@yahoo.pl>
2012-10-30 16:18:43 +0100

    user-stories + few layout changes

--- src/com/example/air_hockey/PlayMultiActivity.java
+++ src/com/example/air_hockey/PlayMultiActivity.java
@@ -8,7 +8,7 @@ public class PlayMultiActivity extends Activity {
     
     public void onCreate(Bundle savedInstanceState) {
     	  super.onCreate(savedInstanceState);
-
+          setContentView(R.layout.activity_playmulti);
 
 
     }
