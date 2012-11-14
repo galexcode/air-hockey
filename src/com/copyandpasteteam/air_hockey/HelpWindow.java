a64f64603c35fa6268e0e98f46e82fec6a3f5cb0
Krzysztof <cypismail@yahoo.pl>
2012-11-14 16:57:10 +0100

    Added back button for Info and Help window

--- src/com/copyandpasteteam/air_hockey/HelpWindow.java
+++ src/com/copyandpasteteam/air_hockey/HelpWindow.java
@@ -2,6 +2,8 @@ package com.copyandpasteteam.air_hockey;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+
 import com.copyandpasteteam.air_hockey.R;
 
 public class HelpWindow extends Activity {
@@ -12,5 +14,11 @@ public class HelpWindow extends Activity {
           setContentView(R.layout.activity_helpwindow); //Uzycie layoutu z activity_helpwindow.xml
 
 
+    }
+    
+    public void endInfo(View view){
+    	
+    	finish();
+    	
     }
 }
--- src/com/copyandpasteteam/air_hockey/InfoWindow.java
+++ src/com/copyandpasteteam/air_hockey/InfoWindow.java
@@ -2,6 +2,8 @@ package com.copyandpasteteam.air_hockey;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+
 import com.copyandpasteteam.air_hockey.R;
 
 public class InfoWindow extends Activity {
@@ -10,7 +12,13 @@ public class InfoWindow extends Activity {
     public void onCreate(Bundle savedInstanceState) {
     	  super.onCreate(savedInstanceState);
           setContentView(R.layout.activity_infowindow); //Uzycie layoutu z activity_infowindow.xml
+          
 
-
+    }
+    
+    public void endInfo(View view){
+    	
+    	finish();
+    	
     }
 }
